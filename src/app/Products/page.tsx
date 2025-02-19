04d141579c7868bd32d03b975fe3a84846ec27ec
Christopher Willians <163358050+christopherfifo@users.noreply.github.com>
2025-02-19 16:03:06 -0300

    configuracao do setup incial do shadcn e adicao suppressHydrationWarning

---
+++ src/app/Products/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const ProductPage = () => {
+  return (
+    <div className="p-5 border border-red-500 rounded-xl">
+      <h1 className="text-red-500">Products pages</h1>
+      <Button>slk numcompensa</Button>
+    </div>
+  );
+};
+
+export default ProductPage;
