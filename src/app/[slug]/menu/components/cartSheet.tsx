d5fa87e78cb02a4f63cde89145069370268042b0
Christopher Willians <163358050+christopherfifo@users.noreply.github.com>
2025-03-16 19:44:41 -0300

    feat: add total calculation and drawer component to cart functionality

--- src/app/[slug]/menu/components/cartSheet.tsx
+++ src/app/[slug]/menu/components/cartSheet.tsx
@@ -7,20 +7,41 @@ import {
 import { useContext } from "react";
 import { CartContext } from "../contexts/cart";
 import CartProductItem from "./cartProductItems";
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Card, CardContent } from "@/components/ui/card";
+import { formatCurrency } from "@/helpers/formatCurrency";
 
 const CartSheet = () => {
-  const { isOpen, toggleCart, products } = useContext(CartContext);
+  const { isOpen, toggleCart, products, total } = useContext(CartContext);
 
   return (
     <Sheet open={isOpen} onOpenChange={toggleCart}>
-      <SheetContent className="w-[80%]">
+      <SheetContent className="w-[86%]">
         <SheetHeader>
           <SheetTitle className="text-left">Sacola</SheetTitle>
         </SheetHeader>
-        <div className="py-5">
-          {products.map((product) => (
-            <CartProductItem key={product.id} product={product} />
-          ))}
+
+        <div className="flex h-full flex-col py-5">
+          <div className="flex-auto overflow-hidden">
+            <ScrollArea className="h-full">
+              <div className="flex flex-col gap-3 py-5 pb-8">
+                {products.map((product) => (
+                  <CartProductItem key={product.id} product={product} />
+                ))}
+              </div>
+            </ScrollArea>
+          </div>
+          <Card className="mb-6">
+            <CardContent className="p-5">
+              <div className="flex justify-between ">                
+                <p className="text-sm text-muted-foreground">Total</p>
+                <p className="text-sm font-semibold">{formatCurrency(total)}</p>
+              </div>
+            </CardContent>
+          </Card>
+
+          <Button className="w-full rounded-full">Finalizar pedido</Button>
         </div>
       </SheetContent>
     </Sheet>
@@ -28,3 +49,5 @@ const CartSheet = () => {
 };
 
 export default CartSheet;
+
+//todo arrumar a responsividade do cartproductitem
